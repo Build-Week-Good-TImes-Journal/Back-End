83fb3cea842937e6f6951ec3299268d55f6785f8
Chris Reyes <creyop1987@gmail.com>
2019-11-14 17:26:05 -0600

    second commit

--- journal/src/RegistrationForm.js
+++ journal/src/RegistrationForm.js
@@ -1,16 +1,41 @@
-import React from 'react';
+Copy
+import React from "react";
+import { withFormik, Form, Field } from "formik";
+import * as Yup from "yup";
 
 
-function RegistrationForm() {
+function LoginForm({ values, errors, touched, isSubmitting }) {
   return (
-    <div className="RegistrationForm">
-      <header className="RegistrationForm-header">
-        <h1>
-           Registration Form
-       </h1>
-      </header>
-    </div>
+    <form>
+      <div>
+        {touched.username && errors.username && <p>{errors.username}</p>}
+        <Field type="username" name="username" placeholder="username" />
+      </div>
+      <div>
+        {touched.password && errors.password && <p>{errors.password}</p>}
+        <Field type="password" name="password" placeholder="Password" />
+      </div>
+    </Form>
   );
 }
 
-export default RegistrationForm;
+const FormikLoginForm = withFormik({
+  mapPropsToValues({ email, password, tos, meal }) {
+    return {
+      login: email || "",
+      password: password || "",
+    };
+  },
+  validationSchema: Yup.object().shape({
+    Login: Yup.string()
+      .email("Email not valid")
+      .required("Email is required"),
+    password: Yup.string()
+      .min(16, "Password must be 16 characters or longer")
+      .required("Password is required")
+  }),
+  handleSubmit(values, { resetForm, setErrors, setSubmitting }) {
+   
+})(LoginForm);
+
+export default FormikLoginForm;
