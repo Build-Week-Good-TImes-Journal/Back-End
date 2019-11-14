1dc4446b24b370d2a7cb386e8e9629f955fd01ad
Chris Reyes <creyop1987@gmail.com>
2019-11-14 12:34:51 -0600

    basic form

---
+++ journal/src/RegistrationForm.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+function RegistrationForm() {
+  return (
+    <div className="RegistrationForm">
+      <header className="RegistrationForm-header">
+        <h1>
+           Registration Form
+       </h1>
+      </header>
+    </div>
+  );
+}
+
+export default RegistrationForm;
