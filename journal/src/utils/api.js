5831d20bc3aafd45b8cac4fb32f9fb0cfa83d0a5
Joe Thompson <joe@pop-os.localdomain>
2019-11-13 22:02:48 -0500

    Added axios and set up api call with authorization header

--- journal/src/utils/api.js
+++ journal/src/utils/api.js
@@ -1,3 +1,14 @@
-function hello() {
-    console.log("hey you guys")
+import axios from "axios";
+
+export function getToken() {
+    return localStorage.getItem("token");
+}
+
+export default function() {
+    return axios.create({
+        baseURL: "http://localhost:4020",
+        headers: {
+            // Authorization: getToken()
+        }
+    });
 }
