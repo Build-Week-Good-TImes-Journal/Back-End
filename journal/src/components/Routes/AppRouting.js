49531519f914e3f2b702b28c2c6d70200c6b12d3
Joe Thompson <joe@pop-os.localdomain>
2019-11-15 20:53:25 -0500

    pulled in login form and created approuting

--- journal/src/components/Routes/AppRouting.js
+++ journal/src/components/Routes/AppRouting.js
@@ -1,3 +1,17 @@
-function hello() {
-    console.log("hey you guys")
+import React from 'react';
+import { Route } from 'react-router-dom';
+import FormikLoginForm from '../Login/Login';
+
+const AppRouting = () => {
+
+    return (
+        <div>
+
+            <Route exact path="/" component={FormikLoginForm} />
+            
+        </div>
+    )
+
 }
+
+export default AppRouting;
