e0c94fcdeeb2e5ae249a87c1d424d862ada348c8
Chris Reyes <creyop1987@gmail.com>
2019-11-20 16:49:26 -0600

    made responsive

--- journal/src/Styles/style-widgets.js
+++ journal/src/Styles/style-widgets.js
@@ -6,7 +6,7 @@ export const Button = styled.button`
   background-color: #0b607b
   color: #FF3238
   border: 1px solid #FFF4A6;
-  margin: 0 auto;
+  margin: 2rem auto;
   &:hover {
     background-color: #28A40E;
     transform: 1rem;
@@ -20,7 +20,7 @@ export const HeaderLogin = styled.h1`
   color: #FFF4A6
   border: 1px solid #FFF4A6;
   width: 7rem;
-  margin: 0 auto;
+  margin: 2rem auto;
   border-radius: 7px;
   background-color: #0b607b;
   padding : 4px;
@@ -32,7 +32,7 @@ export const HeaderLogin = styled.h1`
 
 export const Info = styled.input`
 display: flex;
-margin: 0 auto;
+margin: 1rem auto;
 font-family: Roboto ;
 color: #FFF4A6
 border: 1px solid #FFF4A6;
@@ -48,21 +48,10 @@ text-align: center;
 `;
 
 export const Container = styled.body`
-    
-    background-color: #477C7C;
-
-    height: 40rem;
-    width: 1000px;
-    border: #28A40E;
-    padding: 2rem;
-    align-content: center;
-    
-  
-
-`;
-
-export const infoDiv = styled.div`
-  display-flex: flex;
- 
-  border: 1px solid black;
+  background-color: #477c7c;
+  height: 40rem;
+  width: auto;
+  border: #28a40e;
+  padding: 2rem;
+  align-content: center;
 `;
