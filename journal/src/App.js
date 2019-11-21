5214fa3f074ff0d13bad1f56d797def1f46151cb
Joe Thompson <51089982+Joe-Thompson@users.noreply.github.com>
2019-11-21 17:48:51 -0500

    Update App.js

--- journal/src/App.js
+++ journal/src/App.js
@@ -1,30 +1,13 @@
 import React from 'react';
 import './App.css';
-import AdminLogin from "./components/AdminLogin"
-import Database from "./components/testdb"
-import SignUp from "./components/Register"
-import Users from "./components/usersList"
-import {Route} from "react-router-dom"
-import User from "./components/user"
-import UserLogin from "./components/UsersLogin"
-import UserDashboard from"./components/UserDashboard"
-import Addactivity from "./components/Addactivity"
-import ReflectionForm from "./components/Reflections/Reflections"
+
 
 
 
 function App() {
   return (
     <div className="App">
-      <Route exact path="/adminlogin" component={AdminLogin}/>
-      <Route exact path="/Userlogin" component={UserLogin}/>
-      <Route exact path="/db" component={Database}/>
-      <Route exact path="/signup" component={SignUp}/>
-      <Route exact path="/users" component={Users}/>
-      <Route exact path="/userdashboard/:id" component={UserDashboard}/>
-      <Route exact path="/users/:id" component={User}/>
-      <Route exact path="/userdashboard/:id/addactivity" component={Addactivity}/>
-      <Route exact path="/AddReflection" component={ReflectionForm}/>
+   
       
     </div>
   );
